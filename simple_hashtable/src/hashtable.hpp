7d4165b2b96aa39682df4e8df406f9e04e520db4
shanbak <me@me.com>
2013-09-10 20:30:02 +0300

    proper buckets - start

--- simple_hashtable/src/hashtable.hpp
+++ simple_hashtable/src/hashtable.hpp
@@ -4,6 +4,29 @@
 #include <vector>
 #include <iostream>
 
+/////////// Element ///////////////////
+
+template<typename Key, typename Value>
+struct Element
+{
+    Element(const Key& key, const Value& value);
+    Key  m_key;
+    Value m_value;
+    Element* m_next = 0;
+};
+
+template<typename Key, typename Value>
+Element<Key, Value>::Element(const Key& key, const Value& value) :
+    m_key(key),
+    m_value(value),
+    m_next(0)
+{
+
+}
+
+
+/////////// HashTable ///////////////////
+
 template<typename Key, typename Value>
 class HashTable
 {
@@ -24,20 +47,17 @@ class HashTable
     private:
         std::function<int(const Key&)> m_hasher;
 
-        std::vector<Value> m_values;
-
-        const std::string m_santinel;
+        std::vector<Element<Key, Value>* > m_values;
 };
 
 template<typename Key, typename Value>
 HashTable<Key, Value>::HashTable(size_t size, const std::function<int(const Key&)>& m_hashingFunction) :
-    m_hasher(m_hashingFunction),
-    m_santinel("***")
+    m_hasher(m_hashingFunction)
 {
     m_values.resize(size);
 
     for ( auto& value : m_values )
-        value = m_santinel;
+        value = 0;
 }
 
 template<typename Key, typename Value>
@@ -45,14 +65,22 @@ void HashTable<Key, Value>::add(const Key& key, const Value& value)
 {
     size_t index = m_hasher(key);
 
-    if (m_values.at(index) == m_santinel)
+    if (m_values.at(index) == 0)
     {
-        m_values.at(index) = (value + " ");
+        m_values.at(index) = new Element<Key, Value>(key, value);
     }
     else
     {
-        std::cout << "\033[1;31m CRASH >> collision at index: " << index << "\033[0m" << std::endl;
-        m_values.at(index) += (value + " ");
+        Element<Key, Value>* newElement = new Element<Key, Value>(key, value);
+
+        Element<Key, Value>* iterator = m_values.at(index);
+
+        while ( iterator->m_next != 0 )
+        {
+            iterator = iterator->m_next;
+        }
+
+        iterator->m_next = newElement;
     }
 }
 
@@ -60,10 +88,21 @@ template<typename Key, typename Value>
 void HashTable<Key, Value>::print() const
 {
     if (m_values.size() == 0)
+    {
         std::cout << "table: <empty>" << std::endl;
+    }
     else
+    {
         std::cout << "table: " << std::endl;
+    }
 
     for ( size_t i = 0; i < m_values.size(); i++)
-        std::cout << "[ " << i << " ] = " << m_values.at(i) << std::endl;
+    {
+        std::cout << "[ " << i << " ] = ";
+
+        for (Element<Key, Value>* it = m_values.at(i); it != 0; it = it->m_next)
+        {
+            std::cout << it->m_value << " ---> ";
+        }
+    }
 }

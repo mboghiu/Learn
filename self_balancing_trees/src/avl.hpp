90132d0941879291882a64e46e8c225ee8904c0b
shanbak <me@me.com>
2013-09-13 18:30:25 +0300

    print and binary tree insert

--- self_balancing_trees/src/avl.hpp
+++ self_balancing_trees/src/avl.hpp
@@ -1,17 +1,11 @@
 #ifndef _AVL_TREE
 #define _AVL_TREE
 
+#include <iostream>
+#include <string>
+#include <queue>
 
-/////////////// Node /////////////////
-
-template<typename T>
-struct Node
-{
-    size_t m_key;
-    T m_value;
-    Node* m_left = 0;
-    Node* m_right = 0;
-};
+#include "node.hpp"
 
 /////////////// AVL /////////////////
 
@@ -25,17 +19,105 @@ class AVL
         void Print() const;
 
     private:
-        Node<T>* m_root;
+        Node<T>* m_root = 0;
 
         void rebalance(Node<T>* subtreeRoot);
         void rotate_right(Node<T>* subtreeRoot);
         void rotate_left(Node<T>* subtreeRoot);
+
+        void _insert(size_t key, const T& value, Node<T>* tree);
 };
 
 
 
 /////////////// AVL implementation /////////////////
 
+
+template<typename T>
+void AVL<T>::insert(size_t key, const T& value)
+{
+    // TODO insert
+    if (m_root == nullptr)
+    {
+        m_root = new Node<T>(key, value, 0);
+        return;
+    }
+        
+    _insert(key, value, m_root);
+}
+
+template<typename T>
+void AVL<T>::_insert(size_t key, const T& value, Node<T>* tree)
+{
+    /* simple insert into a binary tree */
+    if (key < tree->GetKey())
+    {
+        if (tree->HasLeftChild())
+            _insert(key, value, tree->GetLeftChild());
+        else
+            tree->AppendLeftChild(new Node<T>(key, value, tree->GetLevel() + 1));
+    }
+    else if (key > tree->GetKey())
+    {
+        if (tree->HasRightChild())
+            _insert(key, value, tree->GetRightChild());
+        else
+            tree->AppendRightChild(new Node<T>(key, value, tree->GetLevel() + 1));
+    }
+    else
+    {
+        std::cout << "Sorry, we don't allow duplicate keys." << std::endl;
+    }
+}
+
+template<typename T>
+void AVL<T>::remove(size_t key)
+{
+    // TODO remove
+    std::cout << key << std::endl;
+}
+
+template<typename T>
+const T& AVL<T>::lookup(size_t key) const
+{
+    // TODO lookup
+    T* value = new T;
+    std::cout << key << std::endl;
+
+    return *value;
+}
+
+template<typename T>
+void AVL<T>::Print() const
+{
+    std::cout << "AVL" << std::endl;
+
+    std::queue<const Node<T>*> toPrint;
+    toPrint.push(m_root);
+
+    int currentLevel = 0;
+
+    while (!toPrint.empty())
+    {
+        const Node<T>* current = toPrint.front();
+        toPrint.pop();
+
+        if (current->GetLevel() > currentLevel)
+        {
+            std::cout << std::endl;
+            currentLevel++;
+        }
+
+        std::cout << current->ToString();
+
+        if (current->HasLeftChild())
+            toPrint.push(current->GetLeftChild());
+        if (current->HasRightChild())
+            toPrint.push(current->GetRightChild());
+    }
+
+    std::cout << std::endl;
+}
 
 
 #endif // _AVL_TREE

7c0284088ff18c538a37808de30f6fd48d336134
shanbak <me@me.com>
2014-04-29 11:16:36 +0100

    trie

--- self_balancing_trees/src/avl.hpp
+++ self_balancing_trees/src/avl.hpp
@@ -21,9 +21,8 @@ class AVL
     private:
         Node<T>* m_root = 0;
 
-        void rebalance(Node<T>* subtreeRoot);
-        void rotate_right(Node<T>* subtreeRoot);
-        void rotate_left(Node<T>* subtreeRoot);
+        void _rotate_right(Node<T>* subtreeRoot);
+        void _rotate_left(Node<T>* subtreeRoot);
 
         void _insert(size_t key, const T& value, Node<T>* tree);
         void _maybe_rebalance(Node<T>* tree);
@@ -44,43 +43,6 @@ void AVL<T>::insert(size_t key, const T& value)
     }
         
     _insert(key, value, m_root);
-}
-
-template<typename T>
-void AVL<T>::_insert(size_t key, const T& value, Node<T>* tree)
-{
-    /* simple insert into a binary tree */
-
-    if (key < tree->GetKey())
-    {
-        if (tree->HasLeftChild())
-            _insert(key, value, tree->GetLeftChild());
-        else
-            tree->AppendLeftChild(new Node<T>(key, value, tree->GetLevel() + 1));
-    }
-    else if (key > tree->GetKey())
-    {
-        if (tree->HasRightChild())
-            _insert(key, value, tree->GetRightChild());
-        else
-            tree->AppendRightChild(new Node<T>(key, value, tree->GetLevel() + 1));
-    }
-    else
-    {
-        std::cout << "Sorry, we don't allow duplicate keys." << std::endl;
-        return;
-    }
-
-    tree->RecomputeHeight();
-
-    _maybe_rebalance(tree);
-}
-
-template<typename T>
-void AVL<T>::_maybe_rebalance(Node<T>* tree)
-{
-    // TODO _maybe_rebalance
-    std::cout << "maybe rebalance " << tree->ToString() << std::endl;
 }
 
 template<typename T>
@@ -132,5 +94,99 @@ void AVL<T>::Print() const
     std::cout << std::endl;
 }
 
+
+/////////////////// Helpers /////////////////////
+
+template<typename T>
+void AVL<T>::_rotate_right(Node<T>* subtreeRoot)
+{
+    /*
+        STEP#1 pivot = Root.OS
+    */ 
+    Node<T>* pivot = subtreeRoot->GetLeftChild();
+
+    assert(pivot->HasLeftChild() or pivot->HasRightChild()); // otherwise we shouldn't be rotating
+    
+    /*
+        STEP#2 Root.OS = pivot.RS (or pivot.OS is only child)
+    */
+    if (pivot->HasRightChild())
+    {
+        subtreeRoot->SetLeftChild(pivot->GetRightChild());
+        /*
+            STEP#3 pivot.RS = root
+        */
+        pivot->SetRightChild(subtreeRoot);
+    }
+    else
+    {
+        subtreeRoot->SetLeftChild(pivot->GetLeftChild());
+        /*
+            STEP#3 pivot.OS = root
+        */
+        pivot->SetLeftChild(subtreeRoot);
+    }
+
+    /*
+        STEP#4 Root = pivot
+    */
+    subtreeRoot = pivot;
+}
+
+template<typename T>
+void AVL<T>::_rotate_left(Node<T>* /*subtreeRoot*/)
+{
+    // TODO rotate left
+}
+
+template<typename T>
+void AVL<T>::_insert(size_t key, const T& value, Node<T>* tree)
+{
+    /* simple insert into a binary tree */
+
+    if (key < tree->GetKey())
+    {
+        if (tree->HasLeftChild())
+            _insert(key, value, tree->GetLeftChild());
+        else
+            tree->AppendLeftChild(new Node<T>(key, value, tree->GetLevel() + 1));
+    }
+    else if (key > tree->GetKey())
+    {
+        if (tree->HasRightChild())
+            _insert(key, value, tree->GetRightChild());
+        else
+            tree->AppendRightChild(new Node<T>(key, value, tree->GetLevel() + 1));
+    }
+    else
+    {
+        std::cout << "Sorry, we don't allow duplicate keys." << std::endl;
+        return;
+    }
+
+    tree->RecomputeHeight();
+
+    _maybe_rebalance(tree);
+}
+
+template<typename T>
+void AVL<T>::_maybe_rebalance(Node<T>* tree)
+{
+    int lf = tree->GetLoadFactor();
+
+    if (lf < -1)
+    {
+        std::cout << "rotate left: " << tree->ToString() << std::endl;
+        _rotate_left(tree);
+        return;
+    }
+ 
+    if (lf > 1)
+    {
+        std::cout << "rotate right: " << tree->ToString() << std::endl;
+        _rotate_right(tree);
+        return;
+    }
+}
 
 #endif // _AVL_TREE

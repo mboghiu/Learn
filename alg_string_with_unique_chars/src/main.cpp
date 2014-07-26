71f109a527c7139dfd28b4c44f4edfe0ef721c36
mboghiu <maria.boghiu@gmail.com>
2014-07-26 20:49:12 +0100

    naive implementation n^2

---
+++ alg_string_with_unique_chars/src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <complex>
+#include <cassert>
+#include <cctype>
+
+void AreEqual(bool expected, bool actual, const std::string& input)
+{
+    if (expected == actual)
+        std::cout << "\033[32mPASS\033[0m" << std::endl;
+    else
+    {
+        std::cout << "\033[31mFAIL\033[0m" << std::endl;
+        std::cout << "input: " << input << std::endl;
+    }
+}
+
+bool AllUnique(const std::string& s)
+{
+    for (size_t x = 0; x < s.length(); x++)
+        for (size_t y = x+1; y < s.length(); y++)
+            if (tolower(s.at(x)) == tolower(s.at(y)))
+                return false;
+
+    return true;
+}
+
+int main()
+{
+    AreEqual(true, AllUnique("abc"), "abc");
+    AreEqual(false, AllUnique("Aa"), "Aa");
+    AreEqual(false, AllUnique("Maria"), "Maria");
+    AreEqual(true, AllUnique("Alex"), "Alex");
+    AreEqual(true, AllUnique("AlEx"), "EAlex");
+    AreEqual(true, AllUnique(""), "<empty string>");
+
+    return 0;
+}

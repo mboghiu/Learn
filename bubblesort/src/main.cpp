4fc03f52962274a2f31a8a31a725e41ef827882c
mboghiu <maria.boghiu@gmail.com>
2014-07-26 16:01:55 +0100

    bubble sort complete

--- bubblesort/src/main.cpp
+++ bubblesort/src/main.cpp
@@ -29,7 +29,14 @@ void AreEqual(const std::vector<T>& expected, const std::vector<T> actual)
 template<typename T>
 std::vector<T> bubble(const std::vector<T>& in)
 {
-    return in; 
+    std::vector<T> v = in;
+
+    for (int x = 0; x < in.size(); x++)
+        for (int y = x + 1; y < in.size(); y++)
+            if (v[x] > v[y])
+                std::swap(v[x], v[y]);
+
+    return std::move(v);
 }
 
 int main()
@@ -41,4 +48,5 @@ int main()
     AreEqual<int>({1}, bubble<int>({1}));
     AreEqual<int>({9,9,9,9,9,9,9}, bubble<int>({9,9,9,9,9,9,9}));
     AreEqual<int>({1,1,3,5,9,11,23}, bubble<int>({5,3,9,11,23,1,1}));
+    AreEqual<int>({1,2,3,4,5,5,7,8,9}, bubble<int>({3,7,8,5,2,1,9,5,4}));
 }

41530ff0cb801631a11eebd45e9d759692dcb3a0
mboghiu <maria.boghiu@gmail.com>
2014-07-29 09:26:33 +0100

    infrastructure

---
+++ alg_linked_list_addition/src/main.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <cstddef>
+#include <sstream>
+
+struct Node
+{
+    Node(int data) :
+        m_next(nullptr),
+        m_data(data)
+    { }
+
+    Node* Add(int data);
+    Node* Delete(int data);
+
+    bool Has(int data) const;
+    int GetData() const;
+
+    size_t Size() const;
+    void Print() const;
+    std::string PrintToString() const;
+
+    Node* m_next;
+    int m_data;
+};
+
+Node* Node::Add(int data)
+{
+    Node* end = this;
+    while (end->m_next != nullptr)
+        end = end->m_next;
+    end->m_next = new Node(data);
+
+    return this;
+}
+
+Node* Node::Delete(int data)
+{
+    if (data == m_data)
+    {
+        Node* ret = m_next;
+        delete this;
+        return ret;
+    }
+
+    for (Node* it = this; it->m_next != nullptr; it = it->m_next)
+        if (it->m_next->m_data == data)
+        {
+            it->m_next = it->m_next->m_next;
+            return this;
+        }
+
+    return nullptr;
+}
+
+bool Node::Has(int data) const
+{
+    if (m_data == data)
+        return true;
+    for (Node* it = m_next; it != nullptr; it = it->m_next)
+        if (it->m_data == data)
+            return true;
+    return false;
+}
+
+int Node::GetData() const
+{
+    return m_data;
+}
+
+size_t Node::Size() const
+{
+    if (m_next == nullptr)
+        return 1;
+
+    size_t c = 1;
+    for (Node* it = m_next; it != nullptr; it = it->m_next)
+        c++;
+    return c;
+}
+
+std::string Node::PrintToString() const
+{
+    std::stringstream ss;
+    ss << m_data << " -> ";
+
+    for (Node* it = m_next; it != nullptr; it = it->m_next)
+        ss << it->m_data << " -> ";
+
+    return std::move(ss.str());
+}
+
+void Node::Print() const
+{
+    std::cout << PrintToString() << std::endl;
+}
+
+//////////// Tester
+
+bool operator==(const Node& lhs, const Node& rhs)
+{
+    if (lhs.Size() != rhs.Size())
+        return false;
+
+    if (lhs.m_data != rhs.m_data)
+        return false;
+
+    Node* itl = lhs.m_next;
+    Node* itr = rhs.m_next;
+    for (; itl != nullptr && itr != nullptr; itl = itl->m_next, itr = itr->m_next) 
+         if (itl->m_data != itr->m_data)
+            return false;
+
+    return true;
+}
+
+template<typename T>
+void Test(const T& expected, const T& actual)
+{
+    if (expected == actual)
+        std::cout << "\033[32mPASS\033[0m" << std::endl;
+    else
+    {
+        std::cout << "\033[31mFAIL\033[0m" << std::endl;
+        std::cout << "expected: " << expected << std::endl;
+        std::cout << "actual: " << actual << std::endl;
+    }
+}
+
+template<>
+void Test(const Node& expected, const Node& actual)
+{
+    if (expected == actual)
+    {
+        std::cout << "\033[32mPASS\033[0m" << std::endl;
+        std::cout << expected.PrintToString() << " == " << actual.PrintToString() << std::endl;
+    }
+    else
+    {
+        std::cout << "\033[31mFAIL\033[0m" << std::endl;
+        std::cout << expected.PrintToString() << " != " << actual.PrintToString() << std::endl;
+    }
+}
+//////////// Adder
+
+Node Add(Node* const x, Node* const y)
+{
+    x->Print();
+    y->Print();
+    return Node(3);
+}
+
+
+int main()
+{
+    Node* c1 = (new Node(3))->Add(1); // 13
+    Node* c2 = (new Node(3))->Add(1)->Add(5); // 513
+    Test<bool>(false, *c1 == *c2);
+
+    Node* d1 = (new Node(3))->Add(1)->Add(5); // 513
+    Node* d2 = (new Node(3))->Add(1)->Add(5); // 513
+    Test<bool>(true, *d1 == *d2);
+    Test<Node>(*d1, *d2);
+
+    Node* e1 = (new Node(3)); // 3
+    Node* e2 = (new Node(3)); // 3
+    Test<bool>(true, *e1 == *e2);
+
+    Node* x = (new Node(3))->Add(1)->Add(5); // 513
+    Node* y = (new Node(5))->Add(9)->Add(2); // 295
+
+    Add(y,x).Print();
+
+    return 0;
+}

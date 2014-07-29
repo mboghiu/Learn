ea4938fc4b3f43664cbabeb6cfa06f087e223732
mboghiu <maria.boghiu@gmail.com>
2014-07-29 20:21:35 +0100

    complete

--- alg_linked_list_addition/src/main.cpp
+++ alg_linked_list_addition/src/main.cpp
@@ -113,42 +113,51 @@ bool operator==(const Node& lhs, const Node& rhs)
     return true;
 }
 
+Node operator+(const Node& rhs, const Node& lhs)
+{
+    Node res((rhs.m_data + lhs.m_data) % 10);
+    int carryOver = (rhs.m_data + lhs.m_data) / 10;
+
+    Node* itl = lhs.m_next;
+    Node* itr = rhs.m_next;
+    while (itl != nullptr && itr != nullptr)
+    {
+        res.Add((itl->m_data + itr->m_data + carryOver) % 10);
+        carryOver = (itl->m_data + itr->m_data + carryOver) / 10; 
+        itl = itl->m_next;
+        itr = itr->m_next;
+    }
+
+    if (itl == nullptr)
+        while (itr != nullptr)
+        {
+            res.Add((itr->m_data + carryOver) % 10);
+            carryOver = (itr->m_data + carryOver) / 10;
+            itr = itr->m_next;
+        }
+    
+    if (itr == nullptr)
+        while (itl != nullptr)
+        {
+            res.Add((itl->m_data + carryOver) % 10);
+            carryOver = (itl->m_data + carryOver) / 10;
+            itl = itl->m_next;
+        }
+    
+    if (carryOver != 0)
+        res.Add(carryOver);
+
+    return std::move(res);
+}
+
 template<typename T>
 void Test(const T& expected, const T& actual)
 {
     if (expected == actual)
         std::cout << "\033[32mPASS\033[0m" << std::endl;
     else
-    {
         std::cout << "\033[31mFAIL\033[0m" << std::endl;
-        std::cout << "expected: " << expected << std::endl;
-        std::cout << "actual: " << actual << std::endl;
-    }
 }
-
-template<>
-void Test(const Node& expected, const Node& actual)
-{
-    if (expected == actual)
-    {
-        std::cout << "\033[32mPASS\033[0m" << std::endl;
-        std::cout << expected.PrintToString() << " == " << actual.PrintToString() << std::endl;
-    }
-    else
-    {
-        std::cout << "\033[31mFAIL\033[0m" << std::endl;
-        std::cout << expected.PrintToString() << " != " << actual.PrintToString() << std::endl;
-    }
-}
-//////////// Adder
-
-Node Add(Node* const x, Node* const y)
-{
-    x->Print();
-    y->Print();
-    return Node(3);
-}
-
 
 int main()
 {
@@ -159,16 +168,31 @@ int main()
     Node* d1 = (new Node(3))->Add(1)->Add(5); // 513
     Node* d2 = (new Node(3))->Add(1)->Add(5); // 513
     Test<bool>(true, *d1 == *d2);
-    Test<Node>(*d1, *d2);
 
     Node* e1 = (new Node(3)); // 3
     Node* e2 = (new Node(3)); // 3
     Test<bool>(true, *e1 == *e2);
 
-    Node* x = (new Node(3))->Add(1)->Add(5); // 513
-    Node* y = (new Node(5))->Add(9)->Add(2); // 295
+    // 513 + 295 = 808
+    Node* x1 = (new Node(3))->Add(1)->Add(5); // 513
+    Node* y1 = (new Node(5))->Add(9)->Add(2); // 295
+    Node* r1 = (new Node(8))->Add(0)->Add(8); // 808
+    x1->Print(); y1->Print(); r1->Print();
+    Test<bool>(true, *r1 == (*x1 + *y1));
 
-    Add(y,x).Print();
+    // 999 + 111 = 1110
+    Node* x2 = (new Node(9))->Add(9)->Add(9);
+    Node* y2 = (new Node(1))->Add(1)->Add(1);
+    Node* r2 = (new Node(0))->Add(1)->Add(1)->Add(1);
+    x2->Print(); y2->Print(); r2->Print();
+    Test<bool>(true, *r2 == (*x2 + *y2));
+
+    // 13 + (-1) = 12
+    Node* x3 = (new Node(3))->Add(1);
+    Node* y3 = (new Node(-1));
+    Node* r3 = (new Node(2))->Add(1);
+    x3->Print(); y3->Print(); r3->Print();
+    Test<bool>(true, *r3 == (*x3 + *y3));
 
     return 0;
 }

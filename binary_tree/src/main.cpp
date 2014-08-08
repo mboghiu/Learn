1bfc0e98596f58da537d5eda6c45508834c8a3be
mboghiu <maria.boghiu@gmail.com>
2014-08-08 16:53:48 +0100

    basic tests

--- binary_tree/src/main.cpp
+++ binary_tree/src/main.cpp
@@ -1,8 +1,17 @@
+#include <string>
+
 #include "test.hpp"
 #include "tree.hpp"
 
 int main()
 {
+    bt<int> tree;
+
+    tree.Insert(3);
+    Test<std::string>("3|", tree.DFTraverse_preorder());
+    Test<std::string>("3|", tree.DFTraverse_inorder());
+    Test<std::string>("3|", tree.DFTraverse_postorder());
+    Test<std::string>("3|", tree.BFTraverse());
 
     return 0;
 }

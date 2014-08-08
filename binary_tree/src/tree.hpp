f03191eebe57a78e8e153a2bf6d5922d601a4428
mboghiu <maria.boghiu@gmail.com>
2014-08-08 17:17:38 +0100

    insert-ish

--- binary_tree/src/tree.hpp
+++ binary_tree/src/tree.hpp
@@ -5,6 +5,7 @@
 
 #include "node.hpp"
 
+
 template<typename T>
 class bt
 {
@@ -22,6 +23,39 @@ class bt
     private:
         Node<T>* _tree = nullptr;
 
-
-
+        void _RecursiveInsert(Node<T>* where, const T& value, bool& done);
 };
+
+template<typename T>
+void bt<T>::Insert(const T& value)
+{
+    bool done = false;
+    _RecursiveInsert(_tree, value, done);
+}
+
+template<typename T>
+void bt<T>::_RecursiveInsert(Node<T>* node, const T& value, bool& done)
+{
+    if (done)
+        return;
+
+    if (node == nullptr)
+    {
+        node = new Node<T>(value);
+        done = true;
+        return;
+    }
+
+    _RecursiveInsert(node->left, value, done);
+    _RecursiveInsert(node->right, value, done);
+}
+
+
+
+
+
+
+
+
+
+

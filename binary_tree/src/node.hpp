2bb29c1f10734d14dbcc9452384bec9a8e368b4a
mboghiu <maria.boghiu@gmail.com>
2014-08-08 16:53:27 +0100

    binary tree node

--- binary_tree/src/node.hpp
+++ binary_tree/src/node.hpp
@@ -3,24 +3,12 @@
 template<typename T>
 struct Node
 {
-    Node() :
-        have(false),
-        next(nullptr)
-    { }
-
     Node(const T& data) :
         data(data),
-        have(true),
         next(nullptr)
-    { }
-
-    Node(const T& data, Node* next) :
-        data(data),
-        have(true),
-        next(next)
     { }
 
     T data;
-    bool have;
-    Node* next;
+    Node* left;
+    Node* right;
 };

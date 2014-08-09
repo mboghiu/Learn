80dd774c348ff90aedb6b1ddd93e3225133b7136
mboghiu <maria.boghiu@gmail.com>
2014-08-09 15:43:51 +0100

    more tests

--- alg_bst_is_balanced/src/main.cpp
+++ alg_bst_is_balanced/src/main.cpp
@@ -10,5 +10,16 @@ int main()
     tree.insert(4);
     test<std::string>("4", tree.inorder());
 
+    tree.insert(6);
+    test<std::string>("46", tree.inorder());
+
+    tree.insert(6);
+    test<std::string>("466", tree.inorder());
+
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(9);
+    test<std::string>("124669", tree.inorder());
+
     return 0;
 }

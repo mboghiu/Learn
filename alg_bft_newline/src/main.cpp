f4fdd427c242cd0e4b7b881901c544d8e2c35329
mboghiu <maria.boghiu@gmail.com>
2014-08-20 12:56:21 +0100

    fb

--- alg_bft_newline/src/main.cpp
+++ alg_bft_newline/src/main.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <string>
 #include <queue>
+#include <cmath>
 
 template<typename T>
 std::string bft_newline(node<T>* root)
@@ -16,16 +17,31 @@ std::string bft_newline(node<T>* root)
     std::queue<node<T>> q;
     q.push(*root);
 
+    int depth = 0;
+    int missing_nodes_next_level = 0;
+    double nodes_on_this_level = std::pow(2, depth) - missing_nodes_next_level;
+
     while(!q.empty())
     {
         auto n = q.front();
-        ss << n.value;
-        if (n.left != nullptr) q.push(*n.left);
-        if (n.right != nullptr) q.push(*n.right);
         q.pop();
+        nodes_on_this_level--;
+
+        ss << n.value << (nodes_on_this_level == 0 ? "\n" : "");
+        if (n.left != nullptr) q.push(*n.left);
+        else missing_nodes_next_level++;
+        if (n.right != nullptr) q.push(*n.right);
+        else missing_nodes_next_level++;
+
+        if (nodes_on_this_level == 0)
+        {
+            depth++;
+            nodes_on_this_level = std::pow(2, depth) - missing_nodes_next_level;
+            missing_nodes_next_level = 0;
+        }
     }
 
-    return std::move(ss.str());
+    return std::move(ss.str().substr(0, ss.str().find_last_of('\n')));
 }
 
 int main()
@@ -34,10 +50,20 @@ int main()
     test<std::string>("3", bft_newline<int>(new node<int>(3)));
     test<std::string>("A", bft_newline<std::string>(new node<std::string>("A")));
 
-    test<std::string>("AKC", bft_newline<std::string>(
+    test<std::string>("A\nKC", bft_newline<std::string>(
             new node<std::string>("A",
                 new node<std::string>("K"),
                 new node<std::string>("C")
+            )));
+
+    test<std::string>("A\nKC\nDE", bft_newline<std::string>(
+            new node<std::string>("A",
+                new node<std::string>("K",
+                    nullptr,
+                    new node<std::string>("D")),
+                new node<std::string>("C",
+                    nullptr,
+                    new node<std::string>("E"))
             )));
 
     return 0;

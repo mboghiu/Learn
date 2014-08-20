8d5457d96c529149a60836db33f0d9a59ef62979
mboghiu <maria.boghiu@gmail.com>
2014-08-20 18:18:44 +0100

    hanoi(3)

--- alg_hanoi/src/main.cpp
+++ alg_hanoi/src/main.cpp
@@ -4,20 +4,20 @@
 #include <sstream>
 #include <stack>
 
-void hanoi(std::stack<int>& start,
+void hanoi(std::stack<int>& start, size_t size,
            std::stack<int>& end,
            std::stack<int>& pivot, std::stringstream& ss)
 {
-    if (start.size() == 0)
+    if (size == 0)
         return;
 
-    if (start.size() == 1)
+    if (size == 1)
     {
         ss << "S->E";
         return;
     }
 
-    if (start.size() == 2)
+    if (size == 2)
     {
         ss << "S->P;";
         pivot.push(start.top());
@@ -33,24 +33,33 @@ void hanoi(std::stack<int>& start,
 
         return;
     }
-
     
+    hanoi(start, size-1, pivot, end, ss);
+
+    ss << "S->E;";
+    end.push(start.top());
+    start.pop();
+
+    hanoi(pivot, pivot.size(), end, start, ss);
 }
 
 int main()
 {
     std::stringstream ss;
     std::stack<int> s, e, p;
+    s.push(3);
     s.push(2);
     s.push(1);
-    hanoi(s, e, p, ss);
-    test<std::string>("S->P;S->E;P->E;", ss.str());
+    hanoi(s, s.size(), e, p, ss);
+    //test<std::string>("S->P;S->E;P->E;", ss.str());
     test<size_t>(0, s.size());
     test<size_t>(0, p.size());
-    test<size_t>(2, e.size());
+    test<size_t>(3, e.size());
     test<size_t>(1, e.top());
     e.pop();
     test<size_t>(2, e.top());
+    e.pop();
+    test<size_t>(3, e.top());
     e.pop();
     test<size_t>(0, e.size());
 

5c8ac5d1386ad307f84044ced13f52503e625c69
mboghiu <maria.boghiu@gmail.com>
2014-08-24 15:22:43 +0100

    left-heavy

--- avl/src/avl.cpp
+++ avl/src/avl.cpp
@@ -19,14 +19,36 @@ void _rotate_left(node*)
 {
 }
 
-void _rotate_right(node*)
+void _rotate_right(node* node)
 {
+    auto x = node;
+    auto y = node->parent;
+    auto z = node->parent->parent;
+
+    if (node->parent->left == node)
+    {
+        // right-rotate
+        auto temp = y->right;
+        y->right = z;
+        z->left = temp;
+    }
+    else
+    {
+        // left-rotate
+        z.left = x;
+        auto temp = x->left;
+        x->left = y;
+        y->right = temp;
+
+        // right-rotate
+        _rotate_right(y);
+    }
 }
 
-node* _find_first_node_with_an_unbalanced_parent(node* n)
+node* _find_first_node_with_an_unbalanced_grandparent(node* n)
 {
-    while (n->parent != nullptr)
-        if (std::abs(n->parent->balance()) == 2)
+    while (n->parent != nullptr && n->parent->parent != nullptr)
+        if (std::abs(n->parent->parent->balance()) == 2)
             return n;
         else
             n = n->parent;
@@ -36,7 +58,7 @@ node* _find_first_node_with_an_unbalanced_parent(node* n)
 
 void _maybe_rebalance(node* node)
 {
-    auto n = _find_first_node_with_an_unbalanced_parent(node);
+    auto n = _find_first_node_with_an_unbalanced_grandparent(node);
 
     if (n == nullptr)
         return;
